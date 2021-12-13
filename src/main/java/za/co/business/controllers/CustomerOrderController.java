6f0365de28e83adf9e2c01fe2392fe437abe1b67
nic0michael <nicomichael2012@gmail.com>
2021-12-13 19:04:17 +0200

    fix imports

--- src/main/java/za/co/business/controllers/CustomerOrderController.java
+++ src/main/java/za/co/business/controllers/CustomerOrderController.java
@@ -1,41 +1,27 @@
 package za.co.business.controllers;
 
 
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import za.co.business.dtos.CustomerOrderRequest;
-import za.co.business.dtos.ProductRequest;
-import za.co.business.dtos.SupplierRequest;
 import za.co.business.logic.BusinessLogicProcessor;
 import za.co.business.model.Customer;
 import za.co.business.model.CustomerOrder;
-import za.co.business.model.Employee;
 import za.co.business.model.Product;
-import za.co.business.model.Supplier;
-import za.co.business.servicemanagers.EmployeeServiceManager;
 import za.co.business.utils.RequestResponseUtils;
 import za.co.business.utils.Utils;
 
